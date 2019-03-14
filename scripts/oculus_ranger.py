d060424bd2f414d7c7bc2327d68615e55a35650d
Pedro Teixeira <pvt@mit.edu>
2019-03-14 14:59:11 -0400

    refactored ranger, added pre-processor support

--- scripts/oculus_ranger.py
+++ scripts/oculus_ranger.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 import numpy as np
 import cv2
+import os.path
 from scipy.interpolate import interp1d
 
+# ROS
 import rospy
 import cv_bridge
 from sonar_oculus.msg import OculusPing
@@ -11,22 +13,45 @@ from sensor_msgs.msg import Image, PointCloud
 from dynamic_reconfigure.server import Server
 from sonar_oculus.cfg import OculusParamsConfig
 
-bridge = cv_bridge.CvBridge()
+# sonar
+from sonar import Sonar
 
-global threshold
-global cloud_pub
-
-# 
 to_rad = lambda bearing: bearing * np.pi / 18000
 
-def ping_callback(msg):
-    global threshold
-    global cloud_pub
+class Ranger:
+  def __init__(self):
+    self.bridge = cv_bridge.CvBridge()
 
-    img = bridge.imgmsg_to_cv2(msg.ping, desired_encoding="passthrough")
+    # initialize sonar pre-processor
+    self.sonar = Sonar()
+    cfg_file = 'config/oculus-m1200d.json'
+    if os.path.isfile(cfg_file):
+      self.sonar.load_config(cfg_file)
+    else:
+      # no config file found
+      rospy.WARN('No sonar config file found; using defaults')
 
-    # TODO: use multibeam here!
+    # subscribe to topics
+    self.ping_sub = rospy.Subscriber('/sonar_oculus_node/ping', OculusPing, self.ping_callback, None, 100)
 
+    # register parameter update callback
+    self.cfg_srv = Server(OculusParamsConfig, self.config_callback)
+
+    # advertise topics
+    self.cloud_publisher = rospy.Publisher('/sonar_oculus_node/cloud', PointCloud, queue_size=50)
+    
+  def ping_callback(self,msg):
+    """
+    Sonar ping callback.
+    """
+    img = self.bridge.imgmsg_to_cv2(msg.ping, desired_encoding="passthrough")
+
+    # TODO: check if updates to sonar config are needed
+    # TODO: save latest config as json
+    
+    # pre-process ping
+    ping = self.sonar.deconvolve(img)
+  
     r = np.linspace(0, msg.fire_msg.range,num=msg.num_ranges)
     az = to_rad(np.asarray(msg.bearings, dtype=np.float32))
 
@@ -35,9 +60,11 @@ def ping_callback(msg):
 
     # image is num_ranges x num_beams
     for beam in range(0, len(az)):
-        idx = np.argmax(img[:,beam])
-        if img[idx,beam] > threshold:
+        idx = np.argmax(ping[:, beam])
+        if ping[idx, beam] > self.threshold:
+            # beam range
             br = idx*msg.range_resolution
+            # beam azimuth
             ba = -az[beam] # TODO: confirm
             pt = Point()
             pt.x = br*np.cos(ba)
@@ -45,20 +72,19 @@ def ping_callback(msg):
             pt.z = 0.0
             cloud_msg.points.append(pt)
                 
-    cloud_pub.publish(cloud_msg)
+    self.cloud_publisher.publish(cloud_msg)
 
-    
-def config_callback(config, level):
-    global threshold
-    threshold = config['Threshold']
+  def config_callback(self, config, level):
+    """
+    Configuration callback.
+    """
+    self.threshold = config['Threshold']
     return config
+ 
 
 if __name__ == '__main__':
     rospy.init_node('oculus_ranger')
-    ping_sub = rospy.Subscriber('/sonar_oculus_node/ping', OculusPing, ping_callback, None, 10)
-    server = Server(OculusParamsConfig, config_callback)
 
-    global cloud_pub
-    cloud_pub = rospy.Publisher('/sonar_oculus_node/cloud',PointCloud, queue_size=50)
+    sonar_ranger = Ranger()
 
     rospy.spin()
